85155e370e8ea4dcd0fb5b9a35a2cb0ee5692deb
timelyportfolio <kent.russell@timelyportfolio.com>
2016-06-14 13:08:16 -0500

    add new shiny roxygen template from `htmlwidgets`

--- R/jsonedit.R
+++ R/jsonedit.R
@@ -94,15 +94,28 @@ jsonedit <- function(
   )
 }
 
-#' Widget output function for use in Shiny
+#' Shiny Bindings for `jsonedit`
+#'
+#' Output and render functions for using jsonedit within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a jsonedit
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name jsonedit-shiny
 #'
 #' @export
 jsoneditOutput <- function(outputId, width = '100%', height = '400px'){
   htmlwidgets::shinyWidgetOutput(outputId, 'jsonedit', width, height, package = 'listviewer')
 }
 
-#' Widget render function for use in Shiny
-#'
+#' @rdname jsonedit-shiny
 #' @export
 renderJsonedit <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
