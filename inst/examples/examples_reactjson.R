7a96a64507f25267f9dbc2445c7fa796d6a1cdf3
timelyportfolio <kent.russell@timelyportfolio.com>
2016-07-08 07:33:18 -0500

    add minimally working [react-json](https://github.com/arqex/react-json) for demonstration purposes

---
+++ inst/examples/examples_reactjson.R
@@ -0,0 +1,24 @@
+library(listviewer)
+
+# use reactR for React dependencies
+# devtools::install_github("timelyportfolio/reactR")
+library(reactR)
+
+reactjson()
+
+reactjson(head(mtcars,4))
+
+library(shiny)
+
+shinyApp(
+  ui = reactjson(
+    list(x=1,msg="react+r+shiny",opts=list(use_react=FALSE)),
+    elementId = "json1"
+  ),
+  server = function(input, output, session){
+    observeEvent(
+      input$json1_change,
+      str(input$json1_change)
+    )
+  }
+)
---
+++ inst/htmlwidgets/reactjson.js
@@ -0,0 +1,40 @@
+HTMLWidgets.widget({
+
+  name: 'reactjson',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+          ReactDOM.render(
+            React.createElement(
+              Json,
+              { value: x.json, onChange: logChange }
+            ),
+            document.body
+          );
+
+          function logChange( value ){
+            console.log( value );
+            if(typeof(Shiny) !== "undefined"){
+              Shiny.onInputChange(el.id + "_change", {value:value});
+            }
+          }
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
