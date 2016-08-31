2971618cf24fe0e6f5b05c5db629ed389f8ca008
jennybc <jenny@stat.ubc.ca>
2016-08-31 16:15:45 -0700

    Add package-level documentation

---
+++ R/listviewer-package.r
@@ -0,0 +1,11 @@
+#' listviewer.
+#'
+#' htmlwidget for interactive views of R lists
+#'
+#' R lists, especially nested lists, can be very difficult to visualize or
+#' represent. \code{str} just isn't enough, so this suite of htmlwidgets is
+#' designed to help see, understand, and maybe even modify your R lists.
+#'
+#' @name listviewer
+#' @docType package
+NULL
